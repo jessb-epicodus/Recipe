fe77885386e29a3f4fdcf351b806502ae71ca204
jb <jessb.epicodus@gmail.com>
2022-03-21 11:47:15 -0700

    build & add controllers

--- RecipeBox/Models/CategoryRecipe.cs
+++ RecipeBox/Models/CategoryRecipe.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 namespace RecipeBox.Models {
   public class CategoryRecipe {
     public int CategoryRecipeId { get; set; }
-    public int CategpryId { get; set; }
+    public int CategoryId { get; set; }
     public int RecipeId { get; set; }
     public virtual Category Category { get; set; }
     public virtual Recipe Recipe { get; set; }
--- RecipeBox/Models/RecipeBoxContext.cs
+++ RecipeBox/Models/RecipeBoxContext.cs
@@ -5,7 +5,7 @@ namespace RecipeBox.Models {
   public class RecipeBoxContext : IdentityDbContext<ApplicationUser> {
     public DbSet<Category> Categories { get; set; }
     public DbSet<Recipe> Recipes { get; set; }
-    public DbSet<CategoryRecipe> CategoryRecipes { get; set; }
+    public DbSet<CategoryRecipe> CategoryRecipe { get; set; }
     public RecipeBoxContext(DbContextOptions options) : base(options) { }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
       optionsBuilder.UseLazyLoadingProxies();
