b1e4113a437734d40920ffeedbb30fa2c86bd195
jb <jessb.epicodus@gmail.com>
2022-03-23 10:21:01 -0700

    working links on ingredient views

--- RecipeBox/Controllers/IngredientsController.cs
+++ RecipeBox/Controllers/IngredientsController.cs
@@ -30,27 +30,27 @@ namespace RecipeBox.Controllers
       return View(userIngredients);
     }
 
-    public ActionResult Create()
-    {
-      ViewBag.RecipeId = new SelectList(_db.Recipes, "RecipeId", "Name");
-      return View();
-    }
+    // public ActionResult Create()
+    // {
+    //   ViewBag.RecipeId = new SelectList(_db.Recipes, "RecipeId", "Name");
+    //   return View();
+    // }
 
-    [HttpPost]
-    public async Task<ActionResult> Create(Ingredient ingredient, int RecipeId)
-    {
-      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-      var currentUser = await _userManager.FindByIdAsync(userId);
-      ingredient.User = currentUser;
-      _db.Ingredients.Add(ingredient);
-      _db.SaveChanges();
-      if (RecipeId != 0)
-      {
-        _db.RecipeIngredient.Add(new RecipeIngredient() { RecipeId = RecipeId, IngredientId = ingredient.IngredientId });
-      }
-      _db.SaveChanges();
-      return RedirectToAction("Index");
-    }
+    // [HttpPost]
+    // public async Task<ActionResult> Create(Ingredient ingredient, int RecipeId)
+    // {
+    //   var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+    //   var currentUser = await _userManager.FindByIdAsync(userId);
+    //   ingredient.User = currentUser;
+    //   _db.Ingredients.Add(ingredient);
+    //   _db.SaveChanges();
+    //   if (RecipeId != 0)
+    //   {
+    //     _db.RecipeIngredient.Add(new RecipeIngredient() { RecipeId = RecipeId, IngredientId = ingredient.IngredientId });
+    //   }
+    //   _db.SaveChanges();
+    //   return RedirectToAction("Index");
+    // }
 
     public ActionResult Details(int id)
     {
@@ -80,46 +80,46 @@ namespace RecipeBox.Controllers
       return RedirectToAction("Index");
     }
 
-    public ActionResult AddRecipe(int id)
-    {
-      var thisIngredient = _db.Ingredients.FirstOrDefault(ingredient => ingredient.IngredientId == id);
-      ViewBag.RecipeId = new SelectList(_db.Recipes, "RecipeId", "Name");
-      return View(thisIngredient);
-    }
+    // public ActionResult AddRecipe(int id)
+    // {
+    //   var thisIngredient = _db.Ingredients.FirstOrDefault(ingredient => ingredient.IngredientId == id);
+    //   ViewBag.RecipeId = new SelectList(_db.Recipes, "RecipeId", "Name");
+    //   return View(thisIngredient);
+    // }
 
-    [HttpPost]
-    public ActionResult AddRecipe(Ingredient ingredient, int RecipeId)
-    {
-      if (RecipeId != 0)
-      {
-      _db.RecipeIngredient.Add(new RecipeIngredient() { RecipeId = RecipeId, IngredientId = ingredient.IngredientId });
-      }
-      _db.SaveChanges();
-      return RedirectToAction("Index");
-    }
+    // [HttpPost]
+    // public ActionResult AddRecipe(Ingredient ingredient, int RecipeId)
+    // {
+    //   if (RecipeId != 0)
+    //   {
+    //   _db.RecipeIngredient.Add(new RecipeIngredient() { RecipeId = RecipeId, IngredientId = ingredient.IngredientId });
+    //   }
+    //   _db.SaveChanges();
+    //   return RedirectToAction("Index");
+    // }
 
-    public ActionResult Delete(int id)
-    {
-      var thisIngredient = _db.Ingredients.FirstOrDefault(ingredient => ingredient.IngredientId == id);
-      return View(thisIngredient);
-    }
+    // public ActionResult Delete(int id)
+    // {
+    //   var thisIngredient = _db.Ingredients.FirstOrDefault(ingredient => ingredient.IngredientId == id);
+    //   return View(thisIngredient);
+    // }
 
-    [HttpPost, ActionName("Delete")]
-    public ActionResult DeleteConfirmed(int id)
-    {
-      var thisIngredient = _db.Ingredients.FirstOrDefault(ingredient => ingredient.IngredientId == id);
-      _db.Ingredients.Remove(thisIngredient);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
-    }
+    // [HttpPost, ActionName("Delete")]
+    // public ActionResult DeleteConfirmed(int id)
+    // {
+    //   var thisIngredient = _db.Ingredients.FirstOrDefault(ingredient => ingredient.IngredientId == id);
+    //   _db.Ingredients.Remove(thisIngredient);
+    //   _db.SaveChanges();
+    //   return RedirectToAction("Index");
+    // }
 
-    [HttpPost]
-    public ActionResult DeleteCategory(int joinId)
-    {
-      var joinEntry = _db.RecipeIngredient.FirstOrDefault(entry => entry.RecipeIngredientId == joinId);
-      _db.RecipeIngredient.Remove(joinEntry);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
-    }
+    // [HttpPost]
+    // public ActionResult DeleteCategory(int joinId)
+    // {
+    //   var joinEntry = _db.RecipeIngredient.FirstOrDefault(entry => entry.RecipeIngredientId == joinId);
+    //   _db.RecipeIngredient.Remove(joinEntry);
+    //   _db.SaveChanges();
+    //   return RedirectToAction("Index");
+    // }
   }
 }
